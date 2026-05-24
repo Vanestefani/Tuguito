6ea824174b5c02400034f56a697cb68c68df6a38
Vanessa Corredor <vanessa.estefania.corredor@gmail.com>
2026-05-24 00:20:31 -0500

    Se añade cinematica,se actualiza modelos

--- Assets/Scripts/Movimiento_Personaje.cs
+++ Assets/Scripts/Movimiento_Personaje.cs
@@ -5,11 +5,14 @@ public class Movimiento_Personaje : MonoBehaviour
 {
     public float velocidad = 5f;
     public float rotacion = 200f;
+
     private Animator animator;
+    private CharacterController controller;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>(); 
     }
 
     void Update()
@@ -18,24 +21,31 @@ public class Movimiento_Personaje : MonoBehaviour
         if (teclado == null) return;
 
         float moverZ = 0f;
-        if (teclado.wKey.isPressed || teclado.upArrowKey.isPressed)        moverZ =  1f;
+        if (teclado.wKey.isPressed || teclado.upArrowKey.isPressed) moverZ = 1f;
         else if (teclado.sKey.isPressed || teclado.downArrowKey.isPressed) moverZ = -1f;
 
         float rotarY = 0f;
-        if (teclado.dKey.isPressed || teclado.rightArrowKey.isPressed)        rotarY =  1f;
-        else if (teclado.aKey.isPressed || teclado.leftArrowKey.isPressed)    rotarY = -1f;
+        if (teclado.dKey.isPressed || teclado.rightArrowKey.isPressed) rotarY = 1f;
+        else if (teclado.aKey.isPressed || teclado.leftArrowKey.isPressed) rotarY = -1f;
 
-    
-        bool estaCaminando     = (moverZ > 0);
+        bool estaCaminando = (moverZ > 0);
         bool estaRetrocediendo = (moverZ < 0);
 
         if (animator != null)
         {
             animator.SetBool("Caminar", estaCaminando);
-          
         }
 
-        transform.Translate(Vector3.forward * moverZ * velocidad * Time.deltaTime, Space.Self);
-        transform.Rotate(Vector3.up * rotarY * rotacion * Time.deltaTime, Space.Self); 
+        
+        transform.Rotate(Vector3.up * rotarY * rotacion * Time.deltaTime, Space.Self);
+
+      
+        Vector3 direccionAvance = transform.forward * moverZ;
+
+      
+        if (controller != null)
+        {
+            controller.Move(direccionAvance * velocidad * Time.deltaTime);
+        }
     }
 }
